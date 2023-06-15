a1c57bccf66f30b1b28ef030b09e8f392ccef073
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-15 13:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+function selected(){
+	let select = document.getElementById("colorSelect")
+	var selectedIndex = select.selectedIndex 
+	select.options[selectedIndex].remove();
+}
